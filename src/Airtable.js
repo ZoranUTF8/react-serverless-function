d156d862cf531717ffe6d6ddb8e690347c6fe734
ZoranUTF8 <janjiczoran23@gmail.com>
2022-01-12 17:02:48 +0100

    Add single product link

--- src/Airtable.js
+++ src/Airtable.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+const dataUrl = "/api/products";
 
 const Airtable = () => {
-    return (
-        <div>
-            <h1>Airtable</h1>
-        </div>
-    )
-}
+  const [products, setProducts] = useState([]);
 
-export default Airtable
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(dataUrl);
+      setProducts(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <section className="section section-center">
+      <div className="title">
+        <h2>Airtable</h2>
+        <div className="title-underline"></div>
+        <div className="products">
+          {products.map((product) => {
+            const { id, url, price, name } = product;
+
+            return (
+              <Link to={`/${id}`} className="product" key={id}>
+                <img src={url} alt={name} />
+                <div className="info">
+                  <h5>{name}</h5>
+                  <h5>{price} $</h5>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Airtable;
---
+++ src/Product.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const Product = () => {
+  
+  return (
+    <div>
+      <h1>single product</h1>
+    </div>
+  );
+};
+
+export default Product;
